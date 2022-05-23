8260e1be87ecd71bf59bcb47d4d910c61a2a6f9d
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-23 11:03:35 +0530

    updated

--- components/Buy.js
+++ components/Buy.js
@@ -1,89 +1,10 @@
-// import React, { useState, useMemo } from "react";
-// import { Keypair, Transaction } from "@solana/web3.js";
-// import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-// import { InfinitySpin } from "react-loader-spinner";
-// import IPFSDownload from "./IpfsDownload";
-
-// export default function Buy({ itemID }) {
-//   const { connection } = useConnection();
-//   const { publicKey, sendTransaction } = useWallet();
-//   const orderID = useMemo(() => Keypair.generate().publicKey, []); // Public key used to identify the order
-
-//   const [paid, setPaid] = useState(null);
-//   const [loading, setLoading] = useState(false); // Loading state of all above
-  
-//   // useMemo is a React hook that only computes the value if the dependencies change
-//   const order = useMemo(
-//     () => ({
-//       buyer: publicKey.toString(),
-//       orderID: orderID.toString(),
-//       itemID: itemID,
-//     }),
-//     [publicKey, orderID, itemID]
-//   );
-
-//   // Fetch the transaction object from the server 
-//   const processTransaction = async () => {
-//     setLoading(true);
-//     const txResponse = await fetch("../api/createTransaction", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(order),
-//     });
-//     const txData = await txResponse.json();
-    
-//     // We create a transaction object
-//     const tx = Transaction.from(Buffer.from(txData.transaction, "base64"));
-//     console.log("Tx data is", tx);
-    
-//     // Attempt to send the transaction to the network
-//     try {
-//       // Send the transaction to the network
-//       const txHash = await sendTransaction(tx, connection);
-//       console.log(`Transaction sent: https://solscan.io/tx/${txHash}?cluster=devnet`);
-//       // Even though this could fail, we're just going to set it to true for now
-//       setPaid(true);
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   if (!publicKey) {
-//     return (
-//       <div>
-//         <p>You need to connect your wallet to make transactions</p>
-//       </div>
-//     );
-//   }
-
-//   if (loading) {
-//     return <InfinitySpin color="gray" />;
-//   }
-
-//   return (
-//     <div>
-//       {paid ? (
-//         <IPFSDownload filename="emojis.zip" hash="QmWWH69mTL66r3H8P4wUn24t1L5pvdTJGUTKBqT11KCHS5" cta="Download emojis"/>
-//       ) : (
-//         <button disabled={loading} className="buy-button" onClick={processTransaction}>
-//           Buy now 🠚
-//         </button>
-//       )}
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect, useMemo } from 'react';
 import { Keypair, Transaction } from '@solana/web3.js';
 import { findReference, FindReferenceError } from '@solana/pay';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { InfinitySpin } from 'react-loader-spinner';
 import IPFSDownload from './IpfsDownload';
-import { addOrder } from '../Lib/api';
+import { addOrder, hasPurchased } from '../lib/api';
 
 const STATUS = {
   Initial: 'Initial',
@@ -136,7 +57,21 @@ export default function Buy({ itemID }) {
   };
 
   useEffect(() => {
-    // Check if transaction was confirmed
+    // Check if this address already has already purchased this item
+    // If so, fetch the item and set paid to true
+    // Async function to avoid blocking the UI
+    async function checkPurchased() {
+      const purchased = await hasPurchased(publicKey, itemID);
+      if (purchased) {
+        setStatus(STATUS.Paid);
+        console.log("Address has already purchased this item!");
+      }
+    }
+    checkPurchased();
+  }, [publicKey, itemID]);
+
+
+  useEffect(() => {
     if (status === STATUS.Submitted) {
       setLoading(true);
       const interval = setInterval(async () => {
