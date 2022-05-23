9b9bf02ca14d7e898b55691899691d174ea972a7
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-23 11:28:01 +0530

    done

--- pages/api/createTransaction.js
+++ pages/api/createTransaction.js
@@ -109,7 +109,7 @@ import BigNumber from "bignumber.js";
 import products from "./products.json";
 
 const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
-const sellerAddress = "B1aLAAe4vW8nSQCetXnYqJfRxzTjnbooczwkUJAr7yMS";
+const sellerAddress = "2ppDSvDbyoc79142Y1y9B7EWRWKW2uvvQSTkSDeuxgTQ";
 const sellerPublicKey = new PublicKey(sellerAddress);
 
 const createTransaction = async (req, res) => {
